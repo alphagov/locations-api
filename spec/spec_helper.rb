35ea76e020e8ab215cb7670398d318c7b07b171f
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2021-08-18 14:36:26 +0100

    Add basic rspec configuration

---
+++ spec/spec_helper.rb
@@ -0,0 +1,16 @@
+ENV["RAILS_ENV"] ||= "test"
+
+require "simplecov"
+SimpleCov.start "rails"
+
+require File.expand_path("../config/environment", __dir__)
+require "rspec/rails"
+
+Dir[Rails.root.join("spec/support/**/*.rb")].sort.each { |f| require f }
+GovukTest.configure
+
+RSpec.configure do |config|
+  config.expose_dsl_globally = false
+  config.infer_spec_type_from_file_location!
+  config.use_transactional_fixtures = true
+end
