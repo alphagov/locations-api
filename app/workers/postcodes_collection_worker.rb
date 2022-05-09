245756630179c1cef6154b528d9e1e9265f0964b
Catalina Ilie <catalina.ilie@digital.cabinet-office.gov.uk>
2022-05-09 15:03:02 +0100

    Release connection before querying for postcodes

--- app/workers/postcodes_collection_worker.rb
+++ app/workers/postcodes_collection_worker.rb
@@ -3,11 +3,13 @@ class PostcodesCollectionWorker
 
   def perform(run_continuously: true)
     loop do
-      postcodes = Postcode.pluck(:postcode)
-
-      postcodes.each do |postcode|
-        ProcessPostcodeWorker.new.perform(postcode)
-        sleep 1
+      ActiveRecord::Base.connection_pool.release_connection
+      ActiveRecord::Base.connection_pool.with_connection do
+        postcodes = Postcode.pluck(:postcode)
+        postcodes.each do |postcode|
+          ProcessPostcodeWorker.new.perform(postcode)
+          sleep 1
+        end
       end
 
       break unless run_continuously
