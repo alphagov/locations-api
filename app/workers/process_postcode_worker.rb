36f524abe08f0942af543c4f8e093ecca6db7dfd
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2022-05-16 11:43:36 +0100

    Log worker errors in Sentry
    We were seeing failures in the workers, but nothing in Sentry.
    I've also back-filled a missing test for the 'postcode update'
    happy path.

--- app/workers/process_postcode_worker.rb
+++ app/workers/process_postcode_worker.rb
@@ -4,5 +4,7 @@ class ProcessPostcodeWorker
   def perform(postcode)
     token_manager = OsPlacesApi::AccessTokenManager.new
     OsPlacesApi::Client.new(token_manager).locations_for_postcode(postcode, update: true)
+  rescue OsPlacesApi::ClientError => e
+    GovukError.notify(e)
   end
 end
