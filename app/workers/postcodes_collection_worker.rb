b4575d39f33a9609245fd394a65f95296e332e11
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2022-05-17 13:45:20 +0100

    Use sidekiq-uniq-jobs and sidekiq-scheduler for predictability
    sidekiq-uniq-jobs prevents duplicate jobs. This is useful, as
    Sidekiq runs on each of the EC2 instances Locations API runs on,
    meaning it is difficult to limit instantiation of workers to
    predictable levels.
    
    We have therefore removed the 'with_advisory_lock' dependency,
    which was the previous attempt at limiting spawned workers to
    one. It never seemed to work very well.
    
    Having PostcodesCollectionWorker spawn on upstart, and being
    responsible for spawning the next PostcodesCollectionWorker in
    the sequence, was brittle. If anything prevented PostcodesCollectionWorker
    from spawning the next PostcodesCollectionWorker, the sequence
    would simply die.
    
    We now use sidekiq-scheduler to spawn a PostcodesCollectionWorker
    every second - which is what we were trying to do by hand before.
    This is much less brittle, and much more accurate. Deploying
    to Integration, we can see exactly 300 PostcodesCollectionWorker
    workers are instantiated over 5 minutes, leading to exactly
    900 ProcessPostcodeWorker workers.

--- app/workers/postcodes_collection_worker.rb
+++ app/workers/postcodes_collection_worker.rb
@@ -1,27 +1,13 @@
 class PostcodesCollectionWorker
   include Sidekiq::Worker
-  sidekiq_options queue: :queue_postcode
+  sidekiq_options queue: :queue_postcode, lock: :until_executed, lock_timeout: nil
 
   POSTCODES_PER_SECOND = 3
 
-  def perform(run_continuously = true) # rubocop:disable Style/OptionalBooleanParameter
-    ApplicationRecord.with_advisory_lock("ProcessPostcodeWorker-single-worker", timeout_seconds: 0) do
-      refresh_oldest_postcodes
-    end
-    process_next_batch if run_continuously
-  end
-
-private
-
-  def refresh_oldest_postcodes
+  def perform
     postcodes = Postcode.uncached do
       Postcode.all.sort_by(&:updated_at).pluck(:postcode).first(POSTCODES_PER_SECOND)
     end
     postcodes.each { |postcode| ProcessPostcodeWorker.perform_async(postcode) }
-  end
-
-  def process_next_batch
-    sleep 1
-    PostcodesCollectionWorker.perform_async(true)
   end
 end
--- spec/workers/postcodes_collection_worker_spec.rb
+++ spec/workers/postcodes_collection_worker_spec.rb
@@ -13,24 +13,7 @@ RSpec.describe PostcodesCollectionWorker do
       expect(ProcessPostcodeWorker).to receive(:perform_async).with("E18QL").ordered
       expect(ProcessPostcodeWorker).not_to receive(:perform_async).with("E18QT")
 
-      PostcodesCollectionWorker.new.perform(false)
-    end
-
-    it "sleeps for 1 second before spawning its replacement" do
-      worker = PostcodesCollectionWorker.new
-      allow(worker).to receive(:sleep) # stop it actually sleeping, for faster tests
-      expect(worker).to receive(:sleep).with(1) # must be '1' for the 'POSTCODES_PER_SECOND' logic to work
-
-      allow(PostcodesCollectionWorker).to receive(:perform_async)
-      expect(PostcodesCollectionWorker).to receive(:perform_async)
-
-      worker.perform(true)
-    end
-
-    it "cannot be invoked multiple times without the first invocation completing" do
-      allow(ApplicationRecord).to receive(:with_advisory_lock)
-      expect(ApplicationRecord).to receive(:with_advisory_lock)
-      PostcodesCollectionWorker.new.perform(false)
+      PostcodesCollectionWorker.new.perform
     end
   end
 end
