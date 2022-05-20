9e4505fd6075d376dd3af3dfbe8149fb7f7ec441
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2022-05-20 09:53:36 +0100

    Install `sentry-sidekiq` gem and remove manual call to GovukError
    In #74, we added a manual call to `GovukError.notify` when an
    exception was raised in Sidekiq. This was because Sentry would
    not receive any errors from Sidekiq.
    
    However, govuk_app_config does support Sidekiq Sentry error
    reporting - it just needs the `sentry-sidekiq` gem adding.
    This is explicitly warned about as of version 4.6.0:
    https://github.com/alphagov/govuk_app_config/blob/main/CHANGELOG.md#460
    
    By adding this dependency, we should be able to remove the
    duplicate config.

--- app/workers/process_postcode_worker.rb
+++ app/workers/process_postcode_worker.rb
@@ -5,7 +5,5 @@ class ProcessPostcodeWorker
   def perform(postcode)
     token_manager = OsPlacesApi::AccessTokenManager.new
     OsPlacesApi::Client.new(token_manager).locations_for_postcode(postcode, update: true)
-  rescue OsPlacesApi::ClientError => e
-    GovukError.notify(e)
   end
 end
--- spec/workers/process_postcode_worker_spec.rb
+++ spec/workers/process_postcode_worker_spec.rb
@@ -10,13 +10,5 @@ RSpec.describe ProcessPostcodeWorker do
 
       ProcessPostcodeWorker.new.perform(postcode)
     end
-
-    it "notifies Sentry when an OS Places API Client exception is raised" do
-      allow(OsPlacesApi::Client).to receive(:new).and_raise(OsPlacesApi::RateLimitExceeded.new)
-
-      expect(GovukError).to receive(:notify).with(OsPlacesApi::RateLimitExceeded)
-
-      ProcessPostcodeWorker.new.perform(postcode)
-    end
   end
 end
