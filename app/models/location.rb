909ab3137d8fcbf4cf1f6976d267a29843b0ee52
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2023-09-25 16:40:02 +0100

    Normalise order of long/lat, add hash representation for Location model

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,12 +1,21 @@
 class Location
   include ActiveModel::Model
 
-  attr_accessor :address, :latitude, :longitude, :local_custodian_code
+  attr_accessor :address, :longitude, :latitude, :local_custodian_code
 
   def ==(other)
     address == other.address &&
-      latitude == other.latitude &&
       longitude == other.longitude &&
+      latitude == other.latitude &&
       local_custodian_code == other.local_custodian_code
+  end
+
+  def to_hash
+    {
+      "address" => address,
+      "longitude" => longitude,
+      "latitude" => latitude,
+      "local_custodian_code" => local_custodian_code,
+    }
   end
 end
