a87f838f2915cb19c39f77d213b7c210d96c7aaa
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2023-11-21 15:53:15 +0000

    Run rails app:update

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,11 @@ module LocationsApi
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w(assets tasks))
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
@@ -30,7 +35,6 @@ module LocationsApi
     #
     config.time_zone = "London"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.autoload_paths << Rails.root.join("lib")
 
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
