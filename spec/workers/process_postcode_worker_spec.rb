d99eceeae50dfd333c9d4f0f499ef5370f89f83c
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2024-04-15 11:00:37 +0100

    Remove manual call to GovukError.notify
    - Remove call
    - Update tests for backoff service (to test properly, and
      maintain coverage

--- spec/workers/process_postcode_worker_spec.rb
+++ spec/workers/process_postcode_worker_spec.rb
@@ -6,17 +6,26 @@ RSpec.describe ProcessPostcodeWorker do
 
     it "updates the given postcode" do
       stubbed_client = double("PostcodeManager")
-
-      expect(PostcodeManager).to receive(:new) { stubbed_client }
       expect(stubbed_client).to receive(:update_postcode).with(postcode)
+      expect(PostcodeManager).to receive(:new) { stubbed_client }
 
       ProcessPostcodeWorker.new.perform(postcode)
     end
 
-    it "notifies Sentry when an OS Places API Client exception is raised" do
+    it "records a success in the backoff service" do
+      stubbed_client = double("PostcodeManager")
+      expect(stubbed_client).to receive(:update_postcode).with(postcode)
+      expect(PostcodeManager).to receive(:new) { stubbed_client }
+
+      expect(Rails.application.config.sidekiq_scheduler_backoff_service).to receive(:record_success)
+
+      ProcessPostcodeWorker.new.perform(postcode)
+    end
+
+    it "records a failure in the backoff service when an OS Places API Client exception is raised" do
       allow(OsPlacesApi::Client).to receive(:new).and_raise(OsPlacesApi::RateLimitExceeded.new)
 
-      expect(GovukError).to receive(:notify).with(OsPlacesApi::RateLimitExceeded)
+      expect(Rails.application.config.sidekiq_scheduler_backoff_service).to receive(:record_failure)
 
       ProcessPostcodeWorker.new.perform(postcode)
     end
