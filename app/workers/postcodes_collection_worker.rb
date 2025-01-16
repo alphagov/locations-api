b770e287cb5f2ec3d45ba045cb5c06bdcae2d93c
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2025-01-16 15:59:37 +0000

    Update PostcodeCollectionWorker to include valid onspd postcodes in the check.
    - Some ONSPD postcodes (small, active) should be eligible for checking against OS Places API, in case higher quality data exists for them.

--- app/workers/postcodes_collection_worker.rb
+++ app/workers/postcodes_collection_worker.rb
@@ -5,9 +5,16 @@ class PostcodesCollectionWorker
   POSTCODES_PER_SECOND = ENV.fetch("OS_PLACES_API_POSTCODES_PER_SECOND", 3).to_i
 
   def perform
-    postcodes = Postcode.uncached do
-      Postcode.os_places.order("updated_at ASC").first(POSTCODES_PER_SECOND).pluck(:postcode)
-    end
     postcodes.each { |postcode| ProcessPostcodeWorker.perform_async(postcode) }
+  end
+
+private
+
+  def postcodes
+    Postcode.uncached do
+      onspd_candidates = Postcode.onspd.active.order("updated_at ASC").select { |r| r.results.first["ONS"]["TYPE"] == "S" }.first(POSTCODES_PER_SECOND)
+      os_places_candidates = Postcode.os_places.order("updated_at ASC").first(POSTCODES_PER_SECOND)
+      (onspd_candidates + os_places_candidates).sort_by(&:updated_at).first(POSTCODES_PER_SECOND).pluck(:postcode)
+    end
   end
 end
--- spec/workers/postcodes_collection_worker_spec.rb
+++ spec/workers/postcodes_collection_worker_spec.rb
@@ -7,15 +7,54 @@ RSpec.describe PostcodesCollectionWorker do
       Postcode.create(postcode: "E18QS", updated_at: 2.days.ago, results: "{}")
       Postcode.create(postcode: "E18QL", updated_at: 1.day.ago, results: "{}")
       Postcode.create(postcode: "E18QT", updated_at: Time.now, results: "{}")
+      stub_const("PostcodesCollectionWorker::POSTCODES_PER_SECOND", 2)
     end
 
     it "creates a ProcessPostcodeWorker for only the oldest postcodes" do
-      stub_const("PostcodesCollectionWorker::POSTCODES_PER_SECOND", 2)
       expect(ProcessPostcodeWorker).to receive(:perform_async).with("E18QS").ordered
       expect(ProcessPostcodeWorker).to receive(:perform_async).with("E18QL").ordered
       expect(ProcessPostcodeWorker).not_to receive(:perform_async).with("E18QT")
 
       PostcodesCollectionWorker.new.perform
+    end
+
+    context "when an ONSPD small active postcode is older than the oldest OS Places postcode" do
+      before do
+        Postcode.create(postcode: "E12AA", source: "onspd", updated_at: 3.days.ago, results: [{ "ONS" => { "TYPE" => "S" } }])
+      end
+
+      it "adds the onspd postcode to the update checker" do
+        expect(ProcessPostcodeWorker).to receive(:perform_async).with("E12AA").ordered
+        expect(ProcessPostcodeWorker).to receive(:perform_async).with("E18QS").ordered
+        expect(ProcessPostcodeWorker).not_to receive(:perform_async).with("E18QL")
+        expect(ProcessPostcodeWorker).not_to receive(:perform_async).with("E18QT")
+
+        PostcodesCollectionWorker.new.perform
+      end
+    end
+
+    context "when an ONSPD small retired postcode is older than the oldest OS Places postcode" do
+      before do
+        Postcode.create(postcode: "E12AA", source: "onspd", updated_at: 3.days.ago, results: [{ "ONS" => { "TYPE" => "S" } }], retired: true)
+      end
+
+      it "does not attempt to update the ONSPD postcode" do
+        expect(ProcessPostcodeWorker).not_to receive(:perform_async).with("E12AA")
+
+        PostcodesCollectionWorker.new.perform
+      end
+    end
+
+    context "when an ONSPD large active postcode is older than the oldest OS Places postcode" do
+      before do
+        Postcode.create(postcode: "E12AA", source: "onspd", updated_at: 3.days.ago, results: [{ "ONS" => { "TYPE" => "L" } }])
+      end
+
+      it "does not attempt to update the ONSPD postcode" do
+        expect(ProcessPostcodeWorker).not_to receive(:perform_async).with("E12AA")
+
+        PostcodesCollectionWorker.new.perform
+      end
     end
   end
 end
